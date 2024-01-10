cac5867eeeac435585676b752172e89fc3781666
Irina <girishkaizh@gmail.com>
2024-01-10 19:51:50 +0400

    feat: add sertificate from react intensive

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,22 @@
-const certificate = document.getElementById('certificate');
+const certificates = document.querySelectorAll('.courses__certificate');
 
-certificate.addEventListener('click', (e) => {
-  certificate.classList.toggle('active');
+certificates.forEach((certificate) => {
+  certificate.addEventListener('click', (e) => {
+    certificate.classList.toggle('active');
 
-  if (!certificate.classList.contains('active')) return;
-  e.stopImmediatePropagation();
+    if (!certificate.classList.contains('active')) return;
+    e.stopImmediatePropagation();
 
-  const mask = document.querySelector('.courses__mask');
-  mask.style.display = 'block';
+    const mask = document.querySelector('.courses__mask');
+    mask.style.display = 'block';
 
-  document.body.addEventListener(
-    'click',
-    () => {
-      certificate.classList.remove('active');
-      mask.style.display = 'none';
-    },
-    { once: true }
-  );
+    document.body.addEventListener(
+      'click',
+      () => {
+        certificate.classList.remove('active');
+        mask.style.display = 'none';
+      },
+      { once: true }
+    );
+  });
 });
